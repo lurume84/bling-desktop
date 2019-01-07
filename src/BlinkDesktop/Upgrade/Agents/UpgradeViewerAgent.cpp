c33ce1fefd00592dbff5c58dc75898e709a17e6a
Luis Ruiz <roco65@gmail.com>
2019-01-07 21:30:10 +0100

    added boost and cef libraries in a single 7z

--- src/BlinkDesktop/Upgrade/Agents/UpgradeViewerAgent.cpp
+++ src/BlinkDesktop/Upgrade/Agents/UpgradeViewerAgent.cpp
@@ -2,7 +2,22 @@
 
 namespace blink { namespace agents {
 	UpgradeViewerAgent::UpgradeViewerAgent()
+	: m_ioService()
+	, m_timer(m_ioService, boost::posix_time::seconds(1000))
 	{
+		m_timer.async_wait([](const boost::system::error_code& ec)
+		{
+		
+		});
 
+		boost::thread t(boost::bind(&boost::asio::io_service::run, &m_ioService));
+		m_backgroundThread.swap(t);
+	}
+
+	UpgradeViewerAgent::~UpgradeViewerAgent()
+	{
+		m_timer.cancel();
+		m_backgroundThread.join();
+		m_ioService.reset();
 	}
 }}
--- src/BlinkDesktop/Upgrade/Agents/UpgradeViewerAgent.h
+++ src/BlinkDesktop/Upgrade/Agents/UpgradeViewerAgent.h
@@ -1,6 +1,17 @@
+#include <malloc.h>
+
+#include <boost/thread.hpp>
+#include <boost/asio.hpp>
+
 namespace blink { namespace agents {
 	class UpgradeViewerAgent
 	{
+	public:
 		UpgradeViewerAgent();
+		~UpgradeViewerAgent();
+	private:
+		boost::asio::io_service		m_ioService;
+		boost::asio::deadline_timer	m_timer;
+		boost::thread				m_backgroundThread;
 	};
 }}
