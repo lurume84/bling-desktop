02151f36146d8fdddb0829f1376ca549b4cff0a7
Luis Ruiz <roco65@gmail.com>
2019-01-30 23:18:23 +0100

    upgradeagent

---
+++ src/BlingApp/Agents/UpgradeAgent.cpp
@@ -0,0 +1,162 @@
+#include "stdafx.h"
+
+#include "NotificationAgent.h"
+
+#include "BlingCore\Upgrade\Events.h"
+
+#include "Toast\ToastEventHandler.h"
+#include "Toast\ToastCommandLineInfo.h"
+#include "Toast\ToastNotificationActivationCallback.h"
+
+namespace bling { namespace ui {  namespace agent {
+
+	NotificationAgent::NotificationAgent()
+	{
+		m_subscriber.subscribe([this](const bling::core::utils::patterns::Event& rawEvt)
+		{
+			auto evt = static_cast<const bling::core::events::DownloadUpgradeEvent&>(rawEvt);
+
+			auto version = utf8toUtf16(evt.m_version);
+
+			ToastEventHandler* handler = new ToastEventHandler([]() {return true; }, []() {return true; }, []() {return true; });
+
+			bling::app::Toast toast(handler);
+			toast.DisplayToast(L"Version " + version + L" available", L"Downloading...", boost::filesystem::canonical("Html/loading/img/logo_download.png").c_str());
+
+		}, bling::core::events::DOWNLOAD_UPGRADE_EVENT);
+
+		m_subscriber.subscribe([this](const bling::core::utils::patterns::Event& rawEvt)
+		{
+			auto evt = static_cast<const bling::core::events::UpgradeCompletedEvent&>(rawEvt);
+
+			auto version = utf8toUtf16(evt.m_version);
+
+			CefRefPtr<CefBrowser> mainBrowser;
+
+			for (auto &browser : browser_list_)
+			{
+				mainBrowser = browser;
+			}
+
+			ToastEventHandler* handler = new ToastEventHandler([&mainBrowser]()
+			{
+				mainBrowser->GetMainFrame()->LoadURL(boost::filesystem::canonical("Html/viewer/index.html").string());
+				return true;
+			}, []() {return true; }, []() {return true; });
+
+			bling::app::Toast toast(handler);
+			toast.DisplayToast(L"Version " + version, L"Succesfully installed new version, click to refresh", boost::filesystem::canonical("Html/loading/img/logo_upgrade.png").c_str());
+
+		}, bling::core::events::UPGRADE_COMPLETED_EVENT);
+	}
+
+	NotificationAgent::~NotificationAgent()
+	{
+		UnregisterActivator();
+	}
+
+	bool NotificationAgent::initialize(boost::optional<bool> doRegister, std::string& reason, int &ecode)
+	{
+		ecode = 0;
+
+		HRESULT hr = m_ToastManager.Create(L"Bling.Desktop");
+
+		//Initialize the Windows Runtime
+		Microsoft::WRL::Wrappers::RoInitializeWrapper winRTInitializer(RO_INIT_MULTITHREADED);
+		{
+			hr = winRTInitializer;
+			if (FAILED(hr))
+			{
+				CString sMsg;
+				sMsg.Format(_T("Failed to initialize Windows Runtime, Error:0x%08X"), hr);
+				reason = sMsg;
+				return false;
+			}
+
+			//Get the executable path
+			std::wstring sModuleName;
+			hr = ToastPP::CManager::GetExecutablePath(sModuleName);
+			if (FAILED(hr))
+			{
+				CString sMsg;
+				sMsg.Format(_T("Failed to get executable path, Error:0x%08X"), hr);
+				reason = sMsg;
+				return false;
+			}
+
+			if(doRegister)
+			{
+				if (*doRegister)
+				{
+					ecode = RegisterCOMServer(sModuleName.c_str());
+					if (FAILED(ecode))
+					{
+						CString sMsg;
+						sMsg.Format(_T("Failed to register COM server, Error:0x%08X"), hr);
+						reason = sMsg;
+						return false;
+					}
+				}
+				else
+				{
+					ecode = UnRegisterCOMServer();
+					if (FAILED(ecode))
+					{
+						CString sMsg;
+						sMsg.Format(_T("Failed to unregister COM server, Error:0x%08X"), ecode);
+						reason = sMsg;
+						return false;
+					}
+				}
+			}
+
+			hr = ToastPP::CManager::RegisterForNotificationSupport(L"Bling Desktop", sModuleName.c_str(), L"Bling.Desktop", __uuidof(ui::toast::ToastNotificationActivationCallback));
+			if (FAILED(hr))
+			{
+				CString sMsg;
+				sMsg.Format(_T("Failed to register for Toast Notifications, Error:0x%08X"), hr);
+				reason = sMsg;
+				return false;
+			}
+
+			ecode = RegisterActivator();
+			if (FAILED(ecode))
+			{
+				CString sMsg;
+				sMsg.Format(_T("Failed to register Activator, Error:0x%08X"), ecode);
+				reason = sMsg;
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	HRESULT NotificationAgent::RegisterCOMServer(PCWSTR pszExePath)
+	{
+		//In this case, just register this application to start
+		return HRESULT_FROM_WIN32(::RegSetKeyValueW(HKEY_CURRENT_USER, L"SOFTWARE\\Classes\\CLSID\\{383803B6-AFDA-4220-BFC3-0DBF810106BF}\\LocalServer32", nullptr, REG_SZ, pszExePath, static_cast<DWORD>(wcslen(pszExePath) * sizeof(wchar_t))));
+	}
+
+	HRESULT NotificationAgent::UnRegisterCOMServer()
+	{
+		HRESULT hr = HRESULT_FROM_WIN32(::RegDeleteKey(HKEY_CURRENT_USER, _T("SOFTWARE\\Classes\\CLSID\\{383803B6-AFDA-4220-BFC3-0DBF810106BF}\\LocalServer32")));
+		if (FAILED(hr))
+			return hr;
+		return HRESULT_FROM_WIN32(::RegDeleteKey(HKEY_CURRENT_USER, _T("SOFTWARE\\Classes\\CLSID\\{383803B6-AFDA-4220-BFC3-0DBF810106BF}")));
+	}
+
+	HRESULT NotificationAgent::RegisterActivator()
+	{
+		Microsoft::WRL::Module<Microsoft::WRL::OutOfProc>::Create([] {});
+		Microsoft::WRL::Module<Microsoft::WRL::OutOfProc>::GetModule().IncrementObjectCount();
+		return Microsoft::WRL::Module<Microsoft::WRL::OutOfProc>::GetModule().RegisterObjects();
+	}
+
+	void NotificationAgent::UnregisterActivator()
+	{
+		Microsoft::WRL::Module<Microsoft::WRL::OutOfProc>::GetModule().UnregisterObjects();
+		Microsoft::WRL::Module<Microsoft::WRL::OutOfProc>::GetModule().DecrementObjectCount();
+	}
+
+}}}
---
+++ src/BlingApp/Agents/UpgradeAgent.h
@@ -0,0 +1,55 @@
+#include "Toast\Toast.h"
+
+#include "BlingCore\Utils\Patterns\PublisherSubscriber\Subscriber.h"
+
+#include <memory>
+
+namespace bling { namespace ui {  
+	
+	namespace toast
+	{
+		class ToastEventHandler;
+	}
+	
+	namespace agent {
+
+	namespace cup = core::utils::patterns;
+
+	const cup::EventType SHOW_NOTIFICATION_EVENT = "SHOW_NOTIFICATION_EVENT";
+
+	class NotificationAgent
+	{
+	public:
+		struct ShowNotificationEvent : public cup::Event
+		{
+			ShowNotificationEvent(std::shared_ptr<ToastPP::CToast> toast, std::shared_ptr<toast::ToastEventHandler> handler)
+				: m_handler(handler)
+				, m_toast(toast)
+			{
+				m_name = SHOW_NOTIFICATION_EVENT;
+			}
+
+			~ShowNotificationEvent();
+
+			std::shared_ptr<ToastPP::CToast> m_toast;
+			std::shared_ptr<toast::ToastEventHandler> m_handler;
+		};
+
+		NotificationAgent();
+		~NotificationAgent();
+
+		bool initialize(boost::optional<bool> doRegister, std::string& reason, int &ecode);
+
+		HRESULT RegisterCOMServer(_In_z_ PCWSTR pszExePath);
+		HRESULT UnRegisterCOMServer();
+		HRESULT RegisterActivator();
+		void UnregisterActivator();
+		
+	private:
+		cup::Subscriber m_subscriber;
+		ToastPP::CManager m_ToastManager;
+
+		std::shared_ptr<ToastPP::CToast> m_toast;
+		std::shared_ptr<toast::ToastEventHandler> m_handler;
+	};
+}}}
